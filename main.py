2b1b3097667bde259c154cc1d927ebec338ea426
Cipher-zam <168484861+Cipher-zam@users.noreply.github.com>
2025-02-20 22:58:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from database import Base,engine
 from routes import users,posts,login
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -24,3 +25,8 @@ Base.metadata.create_all(engine)
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(login.router)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render provides a PORT dynamically
+    uvicorn.run(app, host="0.0.0.0", port=port)
