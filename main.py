50e8bf17775aa9f844cae982310ebe72cd5451f3
Cipher-zam <168484861+Cipher-zam@users.noreply.github.com>
2025-02-21 15:03:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,3 @@ app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(login.router)
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render provides a PORT dynamically
-    uvicorn.run(app, host="0.0.0.0", port=port)
